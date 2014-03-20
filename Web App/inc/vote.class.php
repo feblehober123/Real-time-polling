8e29dd23ea442517c7a27c897203b92fdd8601bf
feblehober123 <sammansfield1@gmail.com>
2014-03-20 12:51:43 -0400

    Added option functions
    Added addOption function
    Added getOptions function
    Added getLastOptionAddTime function

--- Web App/inc/vote.class.php
+++ Web App/inc/vote.class.php
@@ -14,6 +14,20 @@ class vote{
 			->execute($db->bind($values));
 	}
 	
+	public static function addOption($option_name='(null)', $option_sessionid='unknown-device', $option_topicID=1){
+		global $db;
+		
+		// Wipe all other options by this person:
+		$values = array('option_sessionid'=>$option_sessionid);
+		$db->prepare('DELETE FROM '.$db->tableName('options').' '.$db->where($values).';')
+			->execute($db->bind($values));
+		
+		// Add new vote
+		$values = array('option_name' => $option_name, 'option_sessionid'=>$option_sessionid, 'option_topicID'=>$option_topicID);	
+		$db->prepare('INSERT INTO '.$db->tableName('options').' '.$db->insert($values).';')
+			->execute($db->bind($values));
+	}
+	
 	public static function getVotes($vote_topicID=1){
 		global $db;
 		
@@ -23,6 +37,22 @@ class vote{
 		$query->execute($values);
 		return $query->fetchAll(PDO::FETCH_ASSOC);
 		 
+	}
+	
+	public static function getOptions($option_topicID=1){	//probably does not work
+		global $db;
+		
+		$values = array('option_topicID' => $option_topicID);	
+			
+		$query = $db->prepare('SELECT * FROM '.$db->tableName('options').' '.$db->where($values).' ORDER BY `option_time` DESC;');
+		$query->execute($values);
+		//return $query->fetchAll(PDO::FETCH_ASSOC);
+		
+		$names = Array();
+		foreach($query->fetchAll(PDO::FETCH_ASSOC) as $num => $name) {
+			$names[$name] = True;	//This orders the names in an array with the format
+		}				//used in show.php.
+		return $names 			//This may not be neccesary.
 	}
 	
 	public static function getVotesCounts($vote_topicID=1){
@@ -45,6 +75,17 @@ class vote{
 		$query->execute($values);
 		$result = $query->fetchAll(PDO::FETCH_ASSOC);
 		return $result[0]['vote_time'];
+	}
+	
+	public static function getLastOptionAddTime($option_topicID=1){
+		global $db;
+		
+		$values = array('option_topicID' => $option_topicID);
+	
+		$query = $db->prepare('SELECT `option_time` FROM '.$db->tableName('options').' '.$db->where($values).' ORDER BY `option_time` DESC LIMIT 0,1;');
+		$query->execute($values);
+		$result = $query->fetchAll(PDO::FETCH_ASSOC);
+		return $result[0]['option_time'];
 	}
 	
 }
