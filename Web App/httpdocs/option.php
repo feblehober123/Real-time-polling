b2b79ce9a6a327a3feed43475032b7c9e264c8e8
feblehober123 <sammansfield1@gmail.com>
2014-03-21 12:48:59 -0400

    Added option file
    Handles option adding AJAX as a couterpart to vote.php.

---
+++ Web App/httpdocs/option.php
@@ -0,0 +1,18 @@
+<?php
+// Get the pages variables set up
+require('../inc/init.inc.php');
+
+// Connect to the database, if it fails leta assume the database isn't installed
+try {	
+  $db = new db('mysql:host='.db_host.';dbname='.db_database,db_username,db_password);}
+} catch (Exception $e) {
+   die('<h1>Need to Install</h1><p>The database is not installed. <a href="install.php">Visit the installer to install it.</a></p>');
+}
+$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING ); // Turn on db errors, so we can debug.
+
+if(isset($_POST['option_name']) && isset($_POST['option_sessionid']) {
+  vote::addOption($_POST['option_name'], $_POST['option_sessionid']);
+} else {
+  echo vote::getLastOptionTime();
+}
+?>
